60d17a92bd7c9bda8d8e64e802be39265c0a6290
chrisalexeev <chrisalexeev@gmail.com>
2022-02-18 17:09:06 -0500

    working dijkstras, messed up some styling stuff

--- pathfinder/index.js
+++ pathfinder/index.js
@@ -16,15 +16,20 @@ document.addEventListener('mousedown', () => drag = true)
 document.addEventListener('mouseup', () => drag = false)
 
 class Square {
-    constructor(elem,x,y) {
+    constructor(id,elem,x,y) {
+        this.id = id;
         this._elem = elem;
         this._neighbors = [];
         this._position = { x: x, y: y }
         this._searched = false;
         this._isWall = false;
+        this.isStart = false;
+        this.isEnd = false;
+        this.distance = Infinity;
 
         this.addEventListeners(elem);
         this.before = null
+        this.weight = 1
     }
 
     addEventListeners(square) {
@@ -34,24 +39,30 @@ class Square {
             }
             switch (mode) {
                 case 'start':
-                    if (startingNode)
+                    if (startingNode) {
                         startingNode._elem.classList.remove('start')
+                        // startingNode.isStart = false;
+                    }
                     this._elem.classList.add('start');
                     if (!startingNode) {
                         document.getElementById('end').checked = true;
                         mode = 'end'
                     }
                     startingNode = this;
+                    // this.isStart = true;
                     break;
                 case 'end':
-                    if (endingNode)
-                        endingNode._elem.classList.remove('end')
+                    if (endingNode) {
+                        endingNode._elem.classList.remove('end');
+                        // endingNode.isEnd = false;
+                    }
                     this._elem.classList.add('end');
                     if (!endingNode) {
                         document.getElementById('wall').checked = true;
                         mode = 'wall'
                     }
                     endingNode = this;
+                    // this.isEnd = true;
                     break;
             }
         })
@@ -61,6 +72,8 @@ class Square {
             }
             if (mode === 'wall' && drag) {
                 this.toggleWall()
+            } else if (mode === 'addWeight' && drag && !this.isWall) {
+                this.addWeight(1)
             }
         })
         square.addEventListener('mousedown',e => {
@@ -69,6 +82,8 @@ class Square {
             }
             if (mode === 'wall') {
                 this.toggleWall()
+            } else if (mode === 'addWeight' && drag && !this.isWall) {
+                this.addWeight(1)
             }
         })
     }
@@ -94,9 +109,12 @@ class Square {
     }
 
     markSearched(isStart) {
+        // this._elem.style.backgroundColor = `hsl(180, 100%,${50-(this.weight * 3)}%)`;
         this._searched = true;
         if (!isStart)
             this._elem.classList.add('visited')
+        
+        this._elem.style.background = `hsl(180, 100%,${53-(this.weight * 3)}%)`
     }
 
     resetSearched() {
@@ -106,7 +124,14 @@ class Square {
 
     toggleWall() {
         this._isWall = !this._isWall;
+        this._elem.style.backgroundColor = null
         this._elem.classList.toggle('blocked');
+    }
+
+    addWeight(w) {
+        this._elem.style.backgroundColor = `hsl(180, 20%,${100-(this.weight * 3)}%)`;
+        console.log(`${this.id}: ${this.weight}`)
+        this.weight += w;
     }
 
     addNeighbor(n) {
@@ -119,12 +144,14 @@ class Square {
         this._elem.classList.remove('blocked')
         this._isWall = false;
         this.before = null;
+        this.distance = Infinity;
+        this.weight = 1;
+        // this.isStart = false;
+        // this.isEnd = false;
         this._elem.style.cursor = 'pointer';
         this._elem.classList.remove('start');
         this._elem.classList.remove('end');
         this._elem.style.backgroundColor = null;
-            
-        
     }
 }
 
@@ -139,7 +166,8 @@ for (let i = 0; i < 20; i++) {
     for (let j = 0; j < 30; j++) {
         let square = document.createElement('div');
         square.classList.add('square');
-        let sqObj = new Square(square,j,i);
+        let id = (i * 30) + j;
+        let sqObj = new Square(id,square,j,i);
         container.appendChild(sqObj.elem);
         squares[i].push(sqObj);
     }
@@ -208,7 +236,7 @@ async function dfs() {
             continue;
         if (node === endingNode) {
             console.log(count)
-            return node;
+            return true;
         }
         node.markSearched(count === 0 ? true : false);
         node.neighbors.forEach(n => {
@@ -221,6 +249,62 @@ async function dfs() {
         await sleep(50)
     }
     return null;
+}
+
+async function dijkstras() {
+    console.log("Starting Dijkstra's");
+    let unvisited = [];
+    let path_values = {};
+    for (let i = 0; i < 20; i++) {
+        for (let j = 0; j < 30; j++) {
+            if (!squares[i][j].isWall) {
+                unvisited.push(squares[i][j]);
+                path_values[squares[i][j].id] = Infinity;
+            }
+            
+        }
+    }
+    
+    path_values[startingNode.id] = 0;
+    console.log(path_values)
+    let cur;
+    let min = Infinity;
+    let minId = -1;
+    for (const [key, value] of Object.entries(path_values)) {
+        if (value < min) {
+            min = value;
+            minId = Number(key) 
+        }
+    }
+    while (unvisited.length > 0) {
+
+        if (minId < 0)
+            break;
+        
+        cur = unvisited.splice(unvisited.indexOf(unvisited.filter(e => e.id == minId)[0]), 1)
+        cur = cur[0]
+
+        cur.neighbors.forEach(n => {
+            if (min + cur.weight < path_values[n.id] && !n.isWall) {
+                path_values[n.id] = min + cur.weight
+                n.before = cur;
+            }
+        })
+        cur.markSearched()
+        min = Infinity;
+        minId = -1;
+        for (const [key, value] of Object.entries(path_values)) {
+            if (unvisited.map(e => e.id).includes(Number(key)) && value < min) {
+                min = value;
+                minId = Number(key) 
+            }
+        }
+        
+        await sleep(25)
+    }
+    console.log(path_values)
+    console.log(`Shortest path: ${path_values[endingNode.id]}, Starting node: ${startingNode.id}, Ending node: ${endingNode.id}`)
+    return true;
 }
 
 function createPath(node) {
@@ -251,6 +335,7 @@ async function goListener() {
                 squares[i][j]._elem.style.cursor = 'unset';
             }
         }
+        
         switch (algoSelect.value) {
             case 'bfs':
                 result = await bfs();
@@ -258,10 +343,13 @@ async function goListener() {
             case 'dfs':
                 result = await dfs();
                 break;
+            case 'dijkstras':
+                result = await dijkstras();
+                break;
         }
         
         if (result) {
-            let path = createPath(result);
+            let path = createPath(endingNode);
             console.log(path)
             await illuminatePath(path);
         }
